cb59c62faa1ef0ca7086c0f7767721419b28b813
metalninja1001 <101802030+metalninja1001@users.noreply.github.com>
2023-10-01 03:29:10 -0400

    Create setup.py

---
+++ SysAdminCompanion/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="SysAdminCompanion",
+    version="1.0",
+    description="A Python script to chat with your Linux system.",
+    author="Fahd Abrahams",
+    author_email="fahd@perpendicular.web.za",
+    packages=["sysadcom"],
+)
